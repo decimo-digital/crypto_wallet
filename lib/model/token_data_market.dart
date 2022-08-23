b0890f0f399ad285b3b68fb7a124b5a2f7c4c0f1
Andrea La Sala <alasala@toduba.it>
2022-08-23 11:53:01 +0200

    aggiunto token_data_market model e chiamata per caricare lista di token dalle API di CoinGecko

---
+++ lib/model/token_data_market.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Token {
+  Token({
+    @required this.id,
+    @required this.name,
+    @required this.symbol,
+    @required this.image,
+    @required this.currentPrice,
+  });
+
+  String? id;
+  String? name;
+  String? symbol;
+  String? image;
+  double? currentPrice;
+
+  factory Token.fromJson(Map<String, dynamic> json) => Token(
+        id: json['id'],
+        name: json['name'],
+        symbol: json['symbol'],
+        image: json['image'],
+        currentPrice: json['currentPrice'],
+      );
+}
---
+++ lib/service/data_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import '../model/token_data_market.dart';
+import 'package:http/http.dart' as http;
+
+class DataService {
+  Future<List<Token>> fetchToken() async {
+    List<Token> tokenList = <Token>[];
+    final response = await http.get(
+      Uri.parse(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false',
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> values = [];
+      values = json.decode(response.body);
+      if (values.isNotEmpty) {
+        for (int i = 0; i < values.length; i++) {
+          if (values[i] != null) {
+            Map<String, dynamic> map = values[i];
+            tokenList.add(Token.fromJson(map));
+          }
+        }
+      }
+      return tokenList;
+    } else {
+      throw Exception('Failed to load token list');
+    }
+  }
+}
