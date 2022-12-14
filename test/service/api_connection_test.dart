996830e0df0552be67939b3d40f1f88f7d0aafe3
smaso <smsimone65@gmail.com>
2022-12-14 10:46:59 +0100

    test: aggiunti altri test

--- test/service/api_connection_test.dart
+++ test/service/api_connection_test.dart
@@ -95,4 +95,117 @@ void main() {
 
     mockConnection.getPurchases();
   });
+
+  test('Testa il recupero dei balance', () async {
+    final cache = GetIt.I.get<CacheService>();
+
+    expect(
+      cache.getStreamedBalances(),
+      emitsInOrder(
+        [{}, predicate<Map<String, dynamic>>((v) => v['btc'] == 123)],
+      ),
+    );
+
+    when(mockClient.get(any)).thenAnswer(
+      (realInvocation) async => Response(
+        jsonEncode({
+          'balances': {
+            'btc': 123,
+            'eth': 123,
+          }
+        }),
+        200,
+      ),
+    );
+
+    mockConnection.getBalances();
+  });
+
+  test('Testa l\'aggiornamento dei favoriti', () {
+    final cache = GetIt.I.get<CacheService>();
+
+    expect(
+      cache.getTokensStream(),
+      emitsInOrder([
+        [],
+        [predicate<Token>((t) => t.id == 'asd' && !(t.isFavorite ?? false))],
+        [predicate<Token>((t) => t.id == 'asd' && (t.isFavorite ?? false))],
+      ]),
+    );
+
+    when(mockClient.get(any)).thenAnswer(
+      (realInvocation) async => Response(
+        jsonEncode([
+          const Token(
+            id: 'asd',
+            name: 'name',
+            symbol: 'symbol',
+            image: 'image',
+            currentPrice: 453,
+          ),
+        ]),
+        200,
+      ),
+    );
+
+    when(mockClient.post(any, body: anyNamed('body'))).thenAnswer(
+      (realInvocation) async => Response(
+        jsonEncode({}),
+        200,
+      ),
+    );
+
+    mockConnection.getTokens();
+
+    mockConnection.setFavorite('asd');
+  });
+
+  test('Testa l\'aggiornamento dei favoriti rimuovendo il token', () {
+    final cache = GetIt.I.get<CacheService>();
+
+    expect(
+      cache.getTokensStream(),
+      emitsInOrder([
+        [],
+        [predicate<Token>((t) => t.id == 'asd' && !(t.isFavorite ?? false))],
+        [predicate<Token>((t) => t.id == 'asd' && (t.isFavorite ?? false))],
+        [predicate<Token>((t) => t.id == 'asd' && !(t.isFavorite ?? false))],
+      ]),
+    );
+
+    when(mockClient.get(any)).thenAnswer(
+      (realInvocation) async => Response(
+        jsonEncode([
+          const Token(
+            id: 'asd',
+            name: 'name',
+            symbol: 'symbol',
+            image: 'image',
+            currentPrice: 453,
+          ),
+        ]),
+        200,
+      ),
+    );
+
+    when(mockClient.post(any, body: anyNamed('body'))).thenAnswer(
+      (realInvocation) async => Response(
+        jsonEncode({}),
+        200,
+      ),
+    );
+
+    when(mockClient.delete(any, body: anyNamed('body'))).thenAnswer(
+      (realInvocation) async => Response(
+        jsonEncode({}),
+        200,
+      ),
+    );
+
+    mockConnection.getTokens();
+
+    mockConnection.setFavorite('asd');
+
+    mockConnection.removeFavorite('asd');
+  });
 }
